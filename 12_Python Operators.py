7d39ec485f51bc10864c17336e1235242038b4aa
Omkar Nale <omkarnale940@gmail.com>
2023-03-05 19:55:12 +0530

    Python - Operators

---
+++ 12_Python Operators.py
@@ -0,0 +1,43 @@
+print("------------------------------------------Python Operators----------------------------------------")
+
+print("Operators are used to perform operations on variables and values.")
+print("In the example below, we use the + operator to add together two values:")
+
+# Example
+
+print("10 + 20 =",10 + 20)
+print("---------------------------------------------------------------------------------------------------")
+print("Python divides the operators in the following groups:")
+
+print("Arithmetic operators : +,-,*,/,%,**,//")
+print("Assignment operators : =,+=,-=,*=,/=,%=,//=,**=,&=,|=,^=,>>=,<<=")
+print("Comparison operators : ==,!=,>,<,>=,<=")
+print("Logical operators : AND OR NOT")
+print("Identity operators : is , is not")
+print("Membership operators : in, not in")
+print("Bitwise operators : &, |, ^, ~, <<, >>")
+
+print("---------------------------------------------------------------------------------------------------")
+
+print("The precedence order is described in the table below, starting with the highest precedence at the top:")
+
+print("Operator	Description")
+print("()	Parentheses")	
+print("**	Exponentiation	")
+print("+x  -x  ~x	Unary plus, unary minus, and bitwise NOT")	
+print("*  /  //  %	Multiplication, division, floor division, and modulus	")
+print("+  -	Addition and subtraction	")
+print("<<  >>	Bitwise left and right shifts	")
+print("&	Bitwise AND	")
+print("^	Bitwise XOR	")
+print("|	Bitwise OR	")
+print("==  !=  >  >=  <  <=  is  is not  in  not in 	Comparisons, identity, and membership operators")	
+print("not	Logical NOT	")
+print("and	AND	")
+print("or	OR")
+
+print("---------------------------------------------------------------------------------------------------")
+
+
+
+
